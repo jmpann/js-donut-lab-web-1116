39b42914fb1bddbb86ce5d59793b768b74b2daa6
Josh Pann <josh.pann@example.com>
2017-01-18 14:49:06 -0500

    done

--- js/donut-counter.js
+++ js/donut-counter.js
@@ -3,7 +3,13 @@
 //declare 2 variables one for guests and one for donuts
 //use prompt method to bring up a dialog box in the browser for the user to insert the number of guests and donuts.
 //use parseInt to convert the users answer from a string to an integer.
+var guests = parseInt(prompt('How many guests at your party?'));
+var donuts = parseInt(prompt('How many donuts are there?'));
 
 //write a conditional to check if there are enough donuts
-
-  //alert the user with a message telling them the numbers of donuts and guests, and if there are enough donuts for each guest.
+if (donuts > guests) {
+  alert('There are '+donuts+' donuts and '+guests+' guests which is plenty for all.');
+} else {
+  alert('Uh oh! There are '+donuts+' donuts and '+guests+' guests which is not enough!');
+}
+/* +variableName+ is another way to interpolate variables into a string" */
